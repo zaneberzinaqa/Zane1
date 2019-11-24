777a779e4ae8e824f64c7cb753b3c66e92895465
zaneberzinaqa <zanei.berzinai@inbox.lv>
2019-11-24 18:15:37 +0200

    treshais majas darbs

--- src/test/java/DelfiTest.java
+++ src/test/java/DelfiTest.java
@@ -27,41 +27,25 @@ public class DelfiTest {
 
     public void titleAndCommentsTest() {
 
-        //Open browser
-
         System.setProperty("webdriver.chrome.driver", "C:/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
         driver.manage().window().maximize();
 
-        //Open Delfi Home page
-
         driver.get("http://rus.delfi.lv");
-
-        //Find 1st Article
 
         WebElement article = driver.findElements(ARTICLE).get(0);
 
-        //Find 1st article title
-
-//        WebElement homePageTitle = driver.findElement(HOME_PAGE_TITLE);
-
         WebElement homePageTitle = article.findElement(HOME_PAGE_TITLE);
-
-        //Save to String
 
         String titleToCompare = homePageTitle.getText();
 
-        //Find comments count
-
         Integer commentsToCompare = 0;
 
-        if (!article.findElements(HOME_PAGE_COMMENTS).isEmpty()) { //maina nozīmi uz pretējo
+        if (!article.findElements(HOME_PAGE_COMMENTS).isEmpty()) {
 
             WebElement homePageComments = article.findElement(HOME_PAGE_COMMENTS);
-
-            //Save to Integer
 
             String commentsToParse = homePageComments.getText();
 
@@ -71,44 +55,21 @@ public class DelfiTest {
 
         }
 
-        //Open Article page
-
         homePageTitle.click();
-
-        //Find article title
 
         String apTitle = driver.findElement(ARTICLE_PAGE_TITLE).getText();
 
-        //Check title
-
         Assertions.assertEquals(titleToCompare, apTitle, "Wrong title on Article page");
-
-
-        //Find comment count
 
         Integer apComments = Integer.valueOf(driver.findElement(ARTICLE_PAGE_COMMENTS).getText().substring(1, driver.findElement(ARTICLE_PAGE_COMMENTS).getText().length() - 1));
 
-
-        // Check comment count
-
         Assertions.assertEquals(commentsToCompare, apComments, "Wrong comment count on Article page");
-
-
-        //Open comments page
 
         driver.findElement(ARTICLE_PAGE_COMMENTS).click();
 
-
-        //Find title
-
         String cpTitle = driver.findElement(COMMENT_PAGE_TITLE).getText();
 
-
-        //Check title
-
         Assertions.assertEquals(titleToCompare, apTitle, "Wrong article name");
-
-        //Get comment count - vajag savaakt sarakstu, mes to jau darijam, sassummet ciparus un tad sanemt iznakumu
 
         List<WebElement> allComments = driver.findElements(COMMENT_COUNT);
 
@@ -120,16 +81,15 @@ public class DelfiTest {
 
             Integer anonimComments = Integer.valueOf(anonimCommentToParse);
 
-            String registratedComments = allComments.get(1).getText();
+            String registeredComments = allComments.get(1).getText();
 
-            registratedComments = registratedComments.substring(1, registratedComments.length() - 1);
+            registeredComments = registeredComments.substring(1, registeredComments.length() - 1);
 
-            Integer registratedComments = Integer.valueOf(registratedComments);
+            Integer registratedComments = Integer.valueOf(registeredComments);
 
-            Integer registratedComments = Integer.sum(anonimComments, registratedComments);
+            Integer registeredComments = Integer.sum(anonimComments, registeredComments);
 
             Assertions.assertEquals(apComments, "Wrong comment count in comment section");
-
 
         driver.close();
     }
